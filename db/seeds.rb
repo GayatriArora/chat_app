74d21e18385bd860fc86900142036d40bd58cb52
gayatriarora <gayatri7arora@gmail.com>
2021-03-28 17:11:16 +0100

    added migration and seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "gayatria", password: "password")
+User.create(username: "abhinav", password: "password")
+User.create(username: "siddharth", password: "password")
+User.create(username: "nikita", password: "password")
+User.create(username: "schumi", password: "password")
