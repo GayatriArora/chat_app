e50ac31e696939544d353e36fb64b0078b850500
gayatriarora <gayatri7arora@gmail.com>
2021-03-28 18:32:55 +0100

    added index action to sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+      @current_user ||
+    end
+
+    def logged_in?
+        
+    end
+    
+
+    def require_user
+        
+    end
+    
+    
 end
